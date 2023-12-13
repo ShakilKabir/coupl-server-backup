1261054517715fd4248ca1430a8c4bce37f63326
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2023-12-13 15:59:27 +0600

    feat:portfolio chart value route created

--- src/alpaca/alpaca.module.ts
+++ src/alpaca/alpaca.module.ts
@@ -2,8 +2,18 @@
 import { Module } from '@nestjs/common';
 import { AlpacaController } from './alpaca.controller';
 import { AlpacaService } from './alpaca.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  PortfolioValue,
+  PortfolioValueSchema,
+} from './schemas/portfolioValue.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: PortfolioValue.name, schema: PortfolioValueSchema },
+    ]),
+  ],
   controllers: [AlpacaController],
   providers: [AlpacaService],
 })
